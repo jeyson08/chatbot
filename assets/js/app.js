ea309c2345468c37b788e0e3890c2104a8b0e7ba
Jeyson Boursault <jeyson.boursault@gmail.com>
2023-05-10 11:15:43 +0200

    answer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,47 @@
-const question = document.querySelector(".question")
-const conversation = document.querySelector(".conversation")
-const form = document.querySelector("form")
-const tabrep = ["oui", "il fait beau", "parce que"]
+const question = document.querySelector(".question");
+const conversation = document.querySelector(".conversation");
+const form = document.querySelector("form");
+const dialog = [
+  {
+    question: "salut",
+    answer: "coucou",
+  },
+  {
+    question: "ça va",
+    answer: "oui",
+  },
+  {
+    question: "pourquoi",
+    answer: "parce que",
+  },
+  {
+    question: "quel temps",
+    answer: "il fait beau",
+  },
+  {
+    question: "où est tu",
+    answer: "boulogne",
+  }
+];
 
-form.addEventListener("submit", (event)=>{
-    event.preventDefault()
-    const input = document.querySelector("input")
-    const ask = document.createElement("div")
-    ask.classList.add("question")
-    conversation.appendChild(ask)
-    ask.innerHTML = input.value
-    tabrep.forEach(function (val) {
-        //mettre le if ici
-        //créer un objet avec une question et une réponse plutot qu'un tableau
-        // if(input.value === "ça va ?"){
-        //     const rep = document.createElement("div")
-        //     rep.classList.add("reponse")
-        //     conversation.appendChild(rep)
-        //     rep.innerHTML = tabrep[0]
-    })
-    if(input.value.toLowerCase().includes("a va")){
-        const rep = document.createElement("div")
-        rep.classList.add("reponse")
-        conversation.appendChild(rep)
-        rep.innerHTML = tabrep[0]
-    }else if(input.value === "quel temps ?") {
-        const rep = document.createElement("div")
-        rep.classList.add("reponse")
-        conversation.appendChild(rep)
-        rep.innerHTML = tabrep[1]
-    }else if(input.value === "pourquoi ?") {
-        const rep = document.createElement("div")
-        rep.classList.add("reponse")
-        conversation.appendChild(rep)
-        rep.innerHTML = tabrep[2]
-    }else {
-        const rep = document.createElement("div")
-        rep.classList.add("reponse")
-        conversation.appendChild(rep)
-        rep.innerHTML = "Je ne peux pas répondre"
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = document.querySelector("input");
+  const ask = document.createElement("div");
+  ask.classList.add("question");
+  conversation.appendChild(ask);
+  ask.innerHTML = input.value;
+  dialog.forEach((val) => {
+    if (input.value.toLowerCase().includes(val.question)) {
+      const rep = document.createElement("div");
+      rep.classList.add("reponse");
+      conversation.appendChild(rep);
+      rep.innerHTML = val.answer;
     }
-})
+  });
+});
+
+const choice = document.getElementById("propositions");
+dialog.forEach((val) => {
+  choice.innerHTML += `<option value ="${val.question}">`;
+});
