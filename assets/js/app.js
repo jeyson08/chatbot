592af0918556dd1ce90e1fabffdb8cca321fbdd9
Jeyson Boursault <jeyson.boursault@gmail.com>
2023-06-08 11:44:09 +0200

    correction app

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,61 +4,15 @@ const reponse = document.querySelector(".reponse")
 fetch("https://api-chatbot-lw0x.onrender.com/api/v1/alldialogs")
   .then(response => response.json())
   .then(data => {
-    data.message.forEach(dialog => {
-      choice.innerHTML += `<option value ="${dialog.question}">`
-    })
-    // req.addEventListener("change", e=> {
-    //   console.log(e.target.value)
-    //   data.message.forEach(dialog => {
-    //     if(e.target.value === dialog.question){
-    //       console.log(dialog.answer)
-    //       reponse.innerHTML = dialog.answer
-    //     }
-    //   })
-    // })
+    data.forEach(dialog => {
+      choice.innerHTML += `<option value="${dialog.question}">${dialog.question}</option>`;
+    });
   })
-  .catch(error => console.log(error))
+  .catch(error => console.log(error));
 
-//API CHUCK NORRIS ANGLAIS
-const chuck = document.getElementById("chuck")
-fetch("https://api.chucknorris.io/jokes/random")
-  .then(response => response.json())
-  // .then(data => console.log(data.value))
-  .then(data => chuck.innerHTML = data.value)
-  .catch(error => console.log(error))
-
-
-
-const question = document.querySelector(".question");
 const conversation = document.querySelector(".conversation");
 const form = document.querySelector("form");
-const dialog = [
-  {
-    question: "salut",
-    answer: "coucou",
-  },
-  {
-    question: "ça va",
-    answer: "oui",
-  },
-  {
-    question: "pourquoi",
-    answer: "parce que",
-  },
-  {
-    question: "quel temps",
-    answer: "il fait beau",
-  },
-  {
-    question: "où est tu",
-    answer: "boulogne",
-  },
-  {
-    question: "hugo",
-    answer: "hugro",
-  }
-];
-
+const dialog = [];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -71,19 +25,28 @@ form.addEventListener("submit", (event) => {
   rep.classList.add("question");
   conversation.appendChild(rep);
   rep.innerHTML = "...";
+
   setTimeout(function () {
-    dialog.forEach((val) => {
-      if (input.value.toLowerCase().includes(val.question)) {
-        rep.classList.add("question");
-        conversation.appendChild(rep);
-        rep.innerHTML = val.answer;
-        req.value = ""
-      }
-    });
+    fetch("https://api-chatbot-lw0x.onrender.com/api/v1/alldialogs")
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(dialog => {
+          if (input.value.toLowerCase().includes(dialog.question.toLowerCase())) {
+            rep.classList.add("question");
+            conversation.appendChild(rep);
+            rep.innerHTML = dialog.answer;
+            req.value = "";
+          }
+        });
+      })
+      .catch(error => console.log(error));
   }, 800);
 });
 
-
-dialog.forEach((val) => {
-  choice.innerHTML += `<option value ="${val.question}">`;
-});
+const chuck = document.getElementById("chuck");
+fetch("https://api.chucknorris.io/jokes/random")
+  .then(response => response.json())
+  .then(data => {
+    chuck.innerHTML = data.value;
+  })
+  .catch(error => console.log(error));
